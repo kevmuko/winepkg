fe8ffdfb24cdff80096602bd2f0eff9420f1763a
Kevin Ko <kevmuko@gmail.com>
2023-02-04 17:58:02 -0800

    setup: fix ubuntu source for setup-wine.sh

--- setup-wine.sh
+++ setup-wine.sh
@@ -7,7 +7,7 @@ apt install --allow-unauthenticated -y wget
 dpkg --add-architecture i386
 mkdir -pm755 /etc/apt/keyrings
 wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 
 apt -o Acquire::AllowInsecureRepositories=true update
 apt install --install-recommends --allow-unauthenticated -y winehq-staging
